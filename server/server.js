a2a505dcd806b94adb0f597b933277516c09263a
brightlightkim <k2289@byu.edu>
2024-01-05 16:03:04 -0700

    update server

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ import express from 'express';
 import mongoose from 'mongoose';
 import 'dotenv/config';
 import bcrypt from 'bcrypt';
+import { nanoid } from 'nanoid';
+import jwt from 'jsonwebtoken';
+import cors from 'cors';
 
 // schema before
 import User from './Schema/User.js';
@@ -13,10 +16,37 @@ let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for e
 let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex for password
 
 server.use(express.json());
+server.use(cors());
 
 mongoose.connect(process.env.DB_LOCATION, {
   autoIndex: true,
 });
+
+const formatDatatooSend = (user) => {
+  const access_token = jwt.sign(
+    { id: user._id },
+    process.env.SECRECT_ACCESS_KEY
+  );
+
+  return {
+    access_token,
+    profile_img: user.personal_info.profile_img,
+    username: user.personal_info.username,
+    fullname: user.personal_info.fullname,
+  };
+};
+
+const generateUsername = async (email) => {
+  let username = email.split('@')[0];
+
+  let isUsernameUnique = await User.exists({
+    'personal_info.username': username,
+  }).then((result) => result);
+
+  isUsernameUnique ? (username += nanoid().substring(0, 5)) : '';
+
+  return username;
+};
 
 server.post('/signup', (req, res) => {
   let { fullname, email, password } = req.body;
@@ -43,8 +73,8 @@ server.post('/signup', (req, res) => {
     });
   }
 
-  bcrypt.hash(password, 10, (err, hashed_password) => {
-    let username = email.split('@')[0];
+  bcrypt.hash(password, 10, async (err, hashed_password) => {
+    let username = await generateUsername();
 
     let user = new User({
       personal_info: { fullname, email, password: hashed_password, username },
@@ -53,7 +83,7 @@ server.post('/signup', (req, res) => {
     user
       .save()
       .then((u) => {
-        return res.status(200).json({ user: u });
+        return res.status(200).json(formatDatatooSend(u));
       })
       .catch((err) => {
         if (err.code == 11000) {
@@ -63,6 +93,36 @@ server.post('/signup', (req, res) => {
         return res.status(500).json({ error: err.message });
       });
   });
+});
+
+server.post('signin', (req, res) => {
+  let { email, password } = req.body;
+
+  // Fine one is a MongoDB function to find the document
+  User.findOne({ 'personal_info.email': email })
+    .then((user) => {
+      if (!user) {
+        return res.status(403).json({ error: 'Email not found' });
+      }
+
+      bcrypt.compare(password, user.personal_info.password, (err, result) => {
+        if (err) {
+          return res
+            .status(403)
+            .json({ error: 'Error occured while login please try again' });
+        }
+
+        if (!result) {
+          return res.status(403).json({ error: 'Incorrect password' });
+        } else {
+          return res.status(200).json(formatDatatooSend(user));
+        }
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(500).json({ error: err.message });
+    });
 });
 
 server.listen(PORT, () => {
